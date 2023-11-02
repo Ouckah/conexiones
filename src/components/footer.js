2cec4ca764340406607c3992498f0d9149d6c259
ouckah <aidan.ouckama@gmail.com>
2023-11-02 19:49:33 -0400

    ✨ Footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div className="flex flex-col justify-start items-start w-full h-72 bg-white drop-shadow-md px-24 py-12">
+            <div className="flex flex-col justify-start items-start w-1/2 h-full gap-3">
+                <h1 className="text-xl font-extrabold uppercase">About Conexiones</h1>
+                <p className="text-xl font-light">
+                    Since the launch of Marina, Ouckah has captivated solvers by providing engaging word and logic games. In 2022, we introduced Gridder. Today, we proudly added Conexiones to the games made by Ouckah for his girlfriend to play. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
