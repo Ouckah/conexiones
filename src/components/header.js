092201d970e819fc37607de3533f4c273e2ab9a4
ouckah <aidan.ouckama@gmail.com>
2023-11-02 17:07:40 -0400

    ✨ Header

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="flex flex-row justify-start items-end w-full h-36 bg-white drop-shadow-md px-24 py-12 gap-5">
+            <h1 className="text-4xl font-extrabold">Conexiones</h1>
+            <h1 className="text-2xl font-light">November 2, 2023</h1>
+        </div>
+    )
+}
+
+export default Header;
