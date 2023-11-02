f840df09a814203e9159041680ba7aee9f7b4237
ouckah <aidan.ouckama@gmail.com>
2023-11-02 17:07:48 -0400

    💄 Home Page UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      
+      <main className='flex flex-col justify-center items-center w-full h-screen bg-purple-300 gap-16'> 
+
+        <div className='flex flex-col justify-center items-center w-full gap-8'>
+          <div className='w-20 h-20 bg-white'/>
+          <h1 className="text-5xl font-extrabold">Conexiones</h1>
+          <p>Agrupar palabras que comparten un hilo común.</p>
+
+          <button className='bg-black text-white font-semibold px-16 py-3 rounded-full'>Play</button>
+        </div>
+
+        <div className='flex flex-col justify-center items-center w-full gap-2'>
+          <h1 className="text-md font-bold">November 2, 2023</h1>
+          <h1 className='text-sm'>By Aidan Ouckama</h1>
+        </div>
+
+      </main>
     </div>
   );
 }
 
-export default App;
+export default Home;
