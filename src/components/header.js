c3c5d121955f9a3c00735c5b0fc6ad65b4c47e33
ouckah <aidan.ouckama@gmail.com>
2023-11-03 00:28:58 -0400

    ✨ Working Date

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,10 @@
+import { formatDate } from "../utils/game";
+
 const Header = () => {
     return (
         <div className="flex flex-row justify-start items-end w-full h-36 bg-white px-24 py-12 gap-5">
             <h1 className="text-4xl font-extrabold">Conexiones</h1>
-            <h1 className="text-2xl font-light">November 2, 2023</h1>
+            <h1 className="text-2xl font-light">{formatDate()}</h1>
         </div>
     )
 }
