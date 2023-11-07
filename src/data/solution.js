b17237dff2deaf9058b1eea24ca48e5e3f1653c8
ouckah <aidan.ouckama@gmail.com>
2023-11-06 19:59:23 -0500

    🥅 Spelling Error

--- src/data/solution.js
+++ src/data/solution.js
@@ -1,6 +1,6 @@
 export const solutions = {
     "inteligente": ["easy", ["informado", "experto", "listo", "astuto"]],
-    "utensilios de cocina": ["medium", ["tenedor", "chuchara", "chuchillo", "plato"]],
+    "utensilios de cocina": ["medium", ["tenedor", "cuchara", "chuchillo", "plato"]],
     "plana y redonda": ["hard", ["CD", "pizza", "frisbee", "vinilo"]],
     "ajedrez": ["impossible", ["peón", "rey", "reina", "caballero"]]
 }
