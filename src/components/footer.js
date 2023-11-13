98f11720527676ef545f7ddf2861df35fbc2373d
Aidan <43829909+Ouckah@users.noreply.github.com>
2023-11-13 17:27:21 -0500

    Update footer.js
    Fix I found by recording a TikTok

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="flex flex-col justify-start items-start w-full h-full gap-3 lg:w-1/2">
                 <h1 className="text-xl font-extrabold uppercase">About Conexiones</h1>
                 <p className="text-xl font-light">
-                    Since the launch of Marina, Ouckah has captivated solvers by providing engaging word and logic games. In 2022, we introduced Gridder. Today, we proudly added Conexiones to the games made by Ouckah for his girlfriend to play. 
+                    Since the launch of Marina, Ouckah has captivated solvers by providing engaging word and logic games. In 2022, we introduced Guessia. Today, we proudly added Conexiones to the games made by Ouckah for his girlfriend to play. 
                 </p>
             </div>
         </div>
